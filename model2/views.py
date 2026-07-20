9c15e464736a6e8244542609dca47ff6a9b7aea7
choljraa <godyouim@gmail.com>
2026-07-20 11:21:26 +0700

    yoyo

--- model2/views.py
+++ model2/views.py
@@ -1,9 +1,14 @@
+import datetime
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse 
 
 
 def index(request):
-    return render(request, "index.html")
+    context = {
+        'title': "My Home Page",
+        'date': datetime.date.today(),
+    }
+    return render(request, "index.html", context)
 
 def about(request):
     return render(request, "about.html")
