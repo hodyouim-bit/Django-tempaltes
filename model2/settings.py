e88ad6293429cd00419fbfe20177cb5f0e837048
choljraa <godyouim@gmail.com>
2026-08-03 09:22:02 +0700

    chore: update settings to use dj_database_url, update requirements.txt and gitignore

--- model2/settings.py
+++ model2/settings.py
@@ -1,3 +1,5 @@
+# pyrefly: ignore [missing-import]
+import dj_database_url
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -64,10 +66,11 @@ WSGI_APPLICATION = "model2.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
